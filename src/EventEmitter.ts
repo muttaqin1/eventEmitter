1b9b3b636e8c59cca9ef04bce2f2cced55c3d011
muttaqin1 <mdmuttaqin20@gmail.com>
2023-06-21 15:40:20 +0600

    Add more functionality in eventEmitter class.

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -1,5 +1,7 @@
-export default class EventEmitter {
-  constructor(private readonly events = new Map<string, Function[]>()) {}
+import IEventEmitter from './IEventEmitter';
+
+export default class EventEmitter implements IEventEmitter {
+  private readonly events = new Map<string, Function[]>();
 
   public on(eventName: string, callback: Function): void {
     if (!eventName) throw new Error('Event name is required');
@@ -16,7 +18,41 @@ export default class EventEmitter {
   }
   public listenersCount(eventName: string): number {
     if (!eventName) throw new Error('Event name is required');
-    if (!this.events.has(eventName)) return 0;
     return (this.events.get(eventName) || []).length;
+  }
+  public removeListener(eventName: string, callback: Function): void {
+    if (!eventName) throw new Error('Event name is required');
+    if (!callback) throw new Error('Callback is required');
+    if (!this.events.has(eventName)) throw new Error('Event does not exist');
+    const listeners = this.events.get(eventName);
+    listeners?.splice(listeners.indexOf(callback), 1);
+  }
+  public removeAllListeners(eventName: string): void {
+    if (!eventName) throw new Error('Event name is required');
+    if (!this.events.has(eventName)) throw new Error('Event does not exist');
+    this.events.delete(eventName);
+  }
+  public once(eventName: string, callback: Function): void {
+    if (!eventName) throw new Error('Event name is required');
+    if (!callback) throw new Error('Callback is required');
+    const onceCallback = (...args: any[]) => {
+      callback(...args);
+      this.removeListener(eventName, onceCallback);
+    };
+    this.on(eventName, onceCallback);
+  }
+  public eventNames(): string[] {
+    return Array.from(this.events.keys());
+  }
+  public listeners(eventName: string): Function[] {
+    if (!eventName) throw new Error('Event name is required');
+    if (!this.events.has(eventName)) throw new Error('Event does not exist');
+    return this.events.get(eventName) || [];
+  }
+  public prependListener(eventName: string, callback: Function): void {
+    if (!eventName) throw new Error('Event name is required');
+    if (!callback) throw new Error('Callback is required');
+    if (!this.events.has(eventName)) this.events.set(eventName, [callback]);
+    else this.events.get(eventName)?.unshift(callback);
   }
 }
--- src/IEventEmitter.ts
+++ src/IEventEmitter.ts
@@ -1,6 +1,11 @@
 export default interface IEventEmitter {
-  events: Map<string, Function[]>;
   on(eventName: string, callback: Function): void;
   emit(eventName: string, ...args: any[]): void;
   listenersCount(eventName: string): number;
+  removeListener(eventName: string, callback: Function): void;
+  removeAllListeners(eventName: string): void;
+  once(eventName: string, callback: Function): void;
+  eventNames(): string[];
+  listeners(eventName: string): Function[];
+  prependListener(eventName: string, callback: Function): void;
 }
